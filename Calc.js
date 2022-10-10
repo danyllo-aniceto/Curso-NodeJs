145aeccbd32e1afc7fa62ed5cabc3065e2d6c7cb
danyllo-aniceto <danyllo.aniceto.carvalho@gmail.com>
2022-10-10 14:43:06 -0300

    Aula 4 - Módulos

---
+++ Calc.js
@@ -0,0 +1,9 @@
+var SomarFunc = require('./soma')
+var SubFunc = require('./sub')
+var MultFunc = require('./mult')
+var DivFunc = require('./div')
+
+console.log(SomarFunc(2, 4))
+console.log(SubFunc(2, 4))
+console.log(MultFunc(2, 4))
+console.log(DivFunc(2, 4))
